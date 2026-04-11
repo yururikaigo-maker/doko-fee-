24990f97294b96ba4a6d19e985caf2a874904226
yururikaigo-maker <yururi.kaigo@gmail.com>
2026-04-11 17:38:31 +0900

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,25 @@
+const CACHE = 'doko-fee-v1';
+const FILES = [
+  './doko_fee.html',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(keys =>
+    Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+  ));
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request))
+  );
+});
